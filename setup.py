63dd2d6b28872950a9d8f097534a10a85b7a1f24
jmarhee <jmarhee@noreply.localhost>
2020-09-17 08:47:32 +0000

    Update 'setup.py'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name='wslrun',
 version='0.1',
 description='Testing Framework powered by Windows Subsystem for Linux (WSL)',
-url='https://gitlab.com/jmarhee/wslrun',
+url='https://git-central.openfunction.co/jmarhee/wslrun',
 author='jmarhee',
 author_email='jmarhee@interiorae.com',
 license='MIT',
